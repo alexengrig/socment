fccbef7b6cac6c8e8b99cd3b54060c44a4429d69
Grig Alex <alexengrigdev@gmail.com>
2021-07-25 20:08:17 +0300

    Create Comment

---
+++ src/main/java/dev/alexengrig/socment/youtube/model/Comment.java
@@ -0,0 +1,12 @@
+package dev.alexengrig.socment.youtube.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    private String text;
+}
